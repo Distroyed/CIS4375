e99ad45a8618dcca52f698bf8b0abd7166437651
Rayomand Hormuzdi <89496341+RayH75@users.noreply.github.com>
2023-10-13 16:24:48 -0500

    Delete Backend/creds.py

--- Backend/creds.py
+++
@@ -1,5 +0,0 @@
-class Creds:
-    conString = 'database-1.cluster-c1nxsg8z29zr.us-east-2.rds.amazonaws.com'
-    userName = 'admin'
-    password = 'SevenFridaySushi'
-    dbName = 'sevenfridaysushi-inv-db'
