fe9b142ec092f13251a9864aa0a7ca44e81de63d
Phat Vo <pmvo2@cougarnet.uh.edu>
2023-11-05 13:53:30 -0600

    Update PieMappedChart.vue

--- frontend/src/components/PieMappedChart.vue
+++ frontend/src/components/PieMappedChart.vue
@@ -13,11 +13,11 @@ import { ref, provide, watch, onBeforeMount  } from 'vue';
 import { useAppStore } from '@/store/app'
 const piniaStore = useAppStore();
 import StoreApi from '@/services/StoreApi';
-const chartData = ref({});
+const chartData = ref([]);
 
-/* onBeforeMount(async () => {
+onBeforeMount(async () => {
   try {
-    const res = (await ManagementApi.getChartData());
+    const res = (await StoreApi.getPieChartData());
     if (res.status == 200) {
       chartData.value = res.data;
     } else {
@@ -28,16 +28,7 @@ const chartData = ref({});
       piniaStore.setSnackBar(error.message + ". Please Contact IT For Support");
     } else piniaStore.setSnackBar("Error in getting data. Please Contact IT For Support")
   }
-  
-  // Initialize chart options based on the fetched data
-  option.value.series[0].data = [
-    { value: chartData.value.MAPPED_ACCOUNT, name: 'Mapped Account'  },
-    { value: chartData.value.TYPE_NEED, name: 'Type Needed' },
-    { value: chartData.value.OWMER_NEED, name: 'Owner Needed' },
-    { value: chartData.value.EXPIRE_SOON, name: 'Expire Soon' },
-  ];
 });
- */
 
 use([
 CanvasRenderer,
@@ -47,48 +38,11 @@ TooltipComponent,
 LegendComponent,
 ]);
 provide(THEME_KEY, 'dark');
-const option = ref(/* {
-title: {
-text: 'Mapped Account Chart',
-left: 'center',
-},
-tooltip: {
-trigger: 'item',
-formatter: '{a} <br/>{b} : {c} ({d}%)',
-},
-legend: {
-orient: 'vertical',
-left: 'left',
-data: ['Mapped Account','Type Needed','Owner Needed','Expire Soon'], //keys
-},
-series: [
-{
-name: 'Mapped Account Chart',
-type: 'pie',
-radius: '65%',
-center: ['50%', '60%'],
-data: [
-{ value: 0, name: 'Mapped Account' },
-{ value: 0, name: 'Type Needed' },
-{ value: 0, name: 'Owner Needed' },
-{ value: 0, name: 'Expire Soon'   },
-],
-color: ['green', 'yellow', 'red', 'orange'],
-animation: true, 
-emphasis: {
-itemStyle: {
-shadowBlur: 10,
-shadowOffsetX: 0,
-shadowColor: 'rgba(0, 0, 0, 0.5)',
-},
-},
-},
-],
-} */
+const option = ref(
 {
   title: {
-    text: 'Referer of a Website',
-    subtext: 'Fake Data',
+    text: 'Number of Supply by Vendor',
+    subtext: 'Sushi, Produce and Other',
     left: 'center'
   },
   tooltip: {
@@ -103,13 +57,7 @@ shadowColor: 'rgba(0, 0, 0, 0.5)',
       name: 'Access From',
       type: 'pie',
       radius: '50%',
-      data: [
-        { value: 1048, name: 'Search Engine' },
-        { value: 735, name: 'Direct' },
-        { value: 580, name: 'Email' },
-        { value: 484, name: 'Union Ads' },
-        { value: 300, name: 'Video Ads' }
-      ],
+      data: chartData,
       emphasis: {
         itemStyle: {
           shadowBlur: 10,
